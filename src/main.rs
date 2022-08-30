05125f4c0ab0589df1285453bfc2fed3e5c34015
MrPancakes39 <salmannhassan39@gmail.com>
2022-08-30 12:08:12 +0300

    Added clear() and pause() functions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
-fn game_setup() {
-    println!("Hello, world!");
+use std::io::{stdin, stdout, Write};
+
+use clearscreen;
+
+fn clear() {
+    clearscreen::clear().expect("couldn't clear the screen");
 }
 
-fn game_loop() {}
+fn pause() {
+    print!("Press Enter to continue...");
+    stdout().flush().unwrap();
+    stdin()
+        .read_line(&mut String::new())
+        .expect("couldn't read input");
+}
+
+fn game_setup() {
+    clear();
+    println!("Hello Screen 1");
+    pause();
+
+    clear();
+    println!("Hello Screen 2");
+    pause();
+}
+
+// fn game_loop() {}
 
 fn main() {
     game_setup();
-    loop {
-        game_loop();
-    }
+    // loop {
+    //     game_loop();
+    // }
 }
