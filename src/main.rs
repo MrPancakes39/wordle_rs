43b83a295690277c8e3707b2b3ce5f0d8b590430
MrPancakes39 <salmannhassan39@gmail.com>
2022-08-30 15:52:31 +0300

    Added compare_words() function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use ansi_term::{Color, Style};
 use clearscreen;
-use std::io::{stdin, stdout, Write};
+use std::{
+    collections::HashMap,
+    io::{stdin, stdout, Write},
+};
 
 fn clear() {
     clearscreen::clear().expect("couldn't clear the screen");
@@ -14,6 +17,54 @@ fn pause() {
         .expect("couldn't read input");
 }
 
+#[derive(Debug, Clone, Copy)]
+enum LetterState {
+    Correct,
+    Present,
+    Absent,
+    Unknown,
+}
+
+fn compare_words(word: &str, to_match: &str) -> Result<[LetterState; 5], String> {
+    // this handles if word isn't 5 letters long
+    // to_match is should always be 5 letters long
+    if word.len() != to_match.len() && to_match.len() == 5 {
+        return Err(format!("'{word}' should be 5 letters long."));
+    }
+
+    // get letters and their count
+    let mut letters: HashMap<char, u32> = HashMap::new();
+    for ch in to_match.chars() {
+        let counter = letters.entry(ch).or_insert(0);
+        *counter += 1;
+    }
+
+    // initialize the array
+    let mut states: [LetterState; 5] = [LetterState::Unknown; 5];
+
+    // find the state of each letter
+    let match_bytes = to_match.as_bytes();
+    for (i, ch) in word.chars().enumerate() {
+        // if the letter isn't in the to_match word
+        // or if it's count is 0
+        if !letters.contains_key(&ch) || *letters.get(&ch).unwrap() == 0 {
+            states[i] = LetterState::Absent;
+        } else {
+            // else we compare the letters
+            let comp: char = match_bytes[i] as char;
+            states[i] = if ch == comp {
+                LetterState::Correct
+            } else {
+                LetterState::Present
+            };
+            // and we decrease the count of the letter to account for duplicates
+            letters.entry(ch).and_modify(|e| *e -= 1);
+        }
+    }
+
+    Ok(states)
+}
+
 fn main() {
     // ====== Game Setup ======
     // configure ansi support for colors on windows.
@@ -21,6 +72,8 @@ fn main() {
     ansi_term::enable_ansi_support().unwrap();
     // setup loop
     let mut should_loop: bool = true;
+    // generate word
+    let unknown_word = String::from("ONSET");
 
     // ====== Game Loop ======
     while should_loop {
@@ -30,6 +83,8 @@ fn main() {
             println!("|                Wordle                 |");
             println!("+---------------------------------------+");
         }
+        let states = compare_words("MOIST", &unknown_word).unwrap();
+        println!("{:?}", states);
         should_loop = false;
     }
 }
