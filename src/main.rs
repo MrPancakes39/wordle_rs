d57065d12aead77076ae5c71d2202694fb5c3a09
MrPancakes39 <salmannhassan39@gmail.com>
2022-08-30 14:52:11 +0300

    Starting from scratch

--- src/main.rs
+++ src/main.rs
@@ -14,61 +14,6 @@ fn pause() {
         .expect("couldn't read input");
 }
 
-#[derive(Clone, Copy)]
-enum LetterState {
-    Correct,
-    Present,
-    Absent,
-    Unknown,
-}
-
-#[derive(Clone, Copy)]
-struct Letter {
-    ch: char,
-    state: LetterState,
-}
-
-#[derive(Clone, Copy)]
-struct Word {
-    word: [Letter; 6],
-}
-
-impl Word {
-    fn new() -> Word {
-        Word {
-            word: [Letter {
-                ch: ' ',
-                state: LetterState::Unknown,
-            }; 6],
-        }
-    }
-}
-
-fn print_letter(letter: &Letter) {
-    let style = match letter.state {
-        LetterState::Correct => Style::new().on(Color::RGB(83, 141, 78)).fg(Color::White),
-        LetterState::Present => Style::new().on(Color::RGB(181, 159, 59)).fg(Color::White),
-        LetterState::Absent => Style::new().on(Color::RGB(58, 58, 60)).fg(Color::White),
-        LetterState::Unknown => Style::new().on(Color::RGB(18, 18, 19)).fg(Color::White),
-    };
-    print!("{}", style.paint(format!(" {} ", letter.ch.to_string())));
-}
-
-fn read_word() -> Result<String, String> {
-    let mut input = String::new();
-    stdin().read_line(&mut input).unwrap();
-    input.truncate(input.trim_end().len());
-
-    let len = input.len();
-    match len {
-        5 => {
-            input.make_ascii_uppercase();
-            Ok(input)
-        }
-        _ => Err("The entered word is not 5 letters long.".to_string()),
-    }
-}
-
 fn main() {
     // ====== Game Setup ======
     // configure ansi support for colors on windows.
@@ -76,44 +21,9 @@ fn main() {
     ansi_term::enable_ansi_support().unwrap();
     // setup loop
     let mut should_loop: bool = true;
-    // generate letters
-    let mut letters: Vec<Letter> = String::from("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-        .chars()
-        .map(|letter| Letter {
-            ch: letter,
-            state: LetterState::Correct,
-        })
-        .collect();
-    // generate tries
-    let tries = 6;
-    let tried_words: [Word; 6] = [Word::new(); 6];
-    // generate word
-    let _word = String::from("Hello");
 
     // ====== Game Loop ======
     while should_loop {
-        clear();
-        let mut should_pause = true;
-        // print header
-        {
-            println!("+---------------------------------------+");
-            println!("|                Wordle                 |");
-            println!("+---------------------------------------+");
-        }
-        // read a word
-        print!("Enter a word: ");
-        stdout().flush().unwrap();
-        match read_word() {
-            Err(e) => println!("{e} try again."),
-            Ok(w) => {
-                let entered_word = w;
-                println!("'{entered_word}'");
-                should_pause = false;
-                should_loop = false;
-            }
-        }
-        if (should_pause) {
-            pause();
-        }
+        should_loop = false;
     }
 }
